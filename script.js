65f00d8485c4340dcf53134c9bb606bd2c2268a5
weslleyka <166857953+weslleyka@users.noreply.github.com>
2025-08-28 17:15:27 -0300

    ...

---
+++ script.js
@@ -0,0 +1,38 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+    {
+        enunciado: "pergunta 2",
+    ]
+
+}
+{
+    enunciado: "pergunta 2",
+    alternativas: [
+        "alternativa 1",
+        "alternativa 2"
+    ]
+}
+{
+    enunciado: "pergunta 3",
+    alternativas: [ 
+        "alternativa 1",
+        "alternativa 2"
+    ]
+}
+
+
+
+
+
