8c332b2d8176e6d093d12866414e9d55081c00cb
weslleyka <166857953+weslleyka@users.noreply.github.com>
2025-09-04 17:19:28 -0300

    ...

--- script.js
+++ script.js
@@ -6,33 +6,50 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "pergunta 1",
+        enunciado: "A IA será capaz de entender emoções humanas de forma verdadeira no futuro?",
         alternativas: [
             "alternativa 1",
             "alternativa 2"
         ]
     }
     {
-        enunciado: "pergunta 2",
+        enunciado: "A IA substituirá completamente os empregos humanos em todas as áreas?",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
     ]
 
 }
 {
-    enunciado: "pergunta 2",
+    enunciado: "A IA será capaz de criar novas invenções e inovações sozinha, sem ajuda humana?",
     alternativas: [
         "alternativa 1",
         "alternativa 2"
     ]
 }
 {
-    enunciado: "pergunta 3",
+    enunciado: "A IA será uma ferramenta útil na resolução de problemas complexos, como mudanças climáticas?",
     alternativas: [ 
         "alternativa 1",
         "alternativa 2"
     ]
 }
+{
+    enunciado: "A IA se tornará consciente e desenvolverá vontade própria no futuro?",
+    alternativas: [ 
+        "alternativa 1",
+        "alternativa 2"
+    ]
+}
+ let atual = 0;
+ let perguntaAtual;
 
+ function mostrapergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaperguntas.textContent = perguntaAtual.enunciado;
+ }
 
-
+ mostrapergunta();
 
 
